b9a04d3a3ed1853ba66193cb32fdc9bef0198d41
DDABO <djibrildab77@gmail.com>
2023-11-08 14:05:20 +0100

    script pour la map

---
+++ BWA/BWA/map/visualisation.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import folium
+from folium.plugins import MarkerCluster
+
+# Créez la carte de base
+m = folium.Map(location=[48.86762, 2.3624], tiles="OpenStreetMap", zoom_start=5)
+
+# Lisez les données
+df = pd.read_csv("restaurants_geo.csv")
+
+# Créez un groupe de marqueurs
+marker_cluster = MarkerCluster().add_to(m)
+
+for i, row in df.iterrows():
+    lat = df.at[i, "lat"]
+    lng = df.at[i, "lng"]
+    restaurant = df.at[i, "restaurant"]
+    street = str(df.at[i, "street"])
+    zip_code = str(df.at[i, "zip"])
+
+    # Créez le texte de la popup avec un lien de réservation
+    popup_text = f"""
+    <b>{restaurant}</b><br>
+    {street}<br>
+    {zip_code}<br>
+    <a href="https://www.facebook.com/login.php">Faire une réservation</a>
+    """
+
+    if restaurant == "McDonalds":
+        color = "blue"
+    else:
+        color = "red"
+
+    # Créez un marqueur personnalisé avec la popup
+    marker = folium.Marker(
+        location=[lat, lng],
+        icon=folium.DivIcon(
+            icon_size=(150, 36),
+            icon_anchor=(0, 0),
+            html=f'<div><span style="color: {color}; font-size: 12pt;">{restaurant}</span></div>',
+        ),
+    )
+    marker.add_to(marker_cluster)
+
+    # Ajoutez la popup à côté du marqueur
+    folium.Popup(popup_text).add_to(marker)
+
+m.save("index.html")
--- BWA/restaurant/models/restaurant.py
+++ BWA/restaurant/models/restaurant.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Restaurant(models.Model):
     raison_sociale = models.CharField(max_length=100)
     addresse = models.CharField(max_length=200)
@@ -9,3 +10,16 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.raison_sociale
+
+
+class Adresse(models.Model):
+    restaurant = models.OneToOneField(Restaurant)
+    street = models.CharField(max_length=200)
+    zip = models.IntegerField()
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=80)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return f"{self.restaurant},{self.street}, {self.city} ({self.country})"
